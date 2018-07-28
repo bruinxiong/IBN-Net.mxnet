7e4913a7327825f383ec77ce4eff598f78c39e6d
Xiong Lin <bruinxiongmac@gmail.com>
2018-07-28 16:28:59 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
-
-## train resnet_ibn-50 
-python -u train_resnet_ibn.py --data-dir data/imagenet --data-type imagenet --depth 50 --batch-size 256 --gpus=0,1,2,3
